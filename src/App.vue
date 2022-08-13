1cbb9472915aa5dc220286b56910dfa2952cc07a
Carlos Martinez <carlosde362@gmail.com>
2022-08-13 11:19:06 +0200

    added input new task component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import InputAddTask from './components/InputAddTask.vue';
+
+const newTask = (newTask: string) => {
+  console.log(newTask);
+};
+</script>
 
 <template>
-  <h1>Hello World!</h1>
+  <main class="w-full h-full flex flex-col">
+    <div class="flex flex-col w-3/4 gap-4 mx-auto">
+      <h1 class="w-full text-center text-3xl md:text-5xl lg:text-5xl">
+        Todo list
+      </h1>
+      <InputAddTask @add:task="newTask($event)" />
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/InputAddTask.vue
@@ -0,0 +1,45 @@
+<template>
+  <form class="flex flex-col w-full" @submit="emitNewTask($event)">
+    <div class="w-full flex flex-row gap-2 items-center">
+      <textarea
+        type="text"
+        sentences="true"
+        autocomplete="off"
+        @keydown.prevent.ctrl.enter="emitNewTask($event)"
+        required
+        wrap="hard"
+        placeholder="Añade un tarea"
+        class="block resize-none h-max w-full p-4 font-system transition transition-delay-100 rounded opacity-70 focus:opacity-100 placeholder:text-white"
+        v-model="newTask"
+      />
+      <button
+        type="submit"
+        class="rounded h-max outline outline-transparent outline-2 outline-offset-4 bg-green-700 p-2 px-4 transition delay-100 hover:bg-green-600 hover:outline-green-700"
+      >
+        Añadir
+      </button>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'InputAddTask',
+  data() {
+    return {
+      newTask: '',
+    };
+  },
+  emits: ['add:task'],
+  methods: {
+    emitNewTask(event: Event) {
+      event.preventDefault();
+      this.$emit('add:task', this.newTask);
+      this.newTask = '';
+    },
+  },
+});
+</script>
+
+<style></style>
