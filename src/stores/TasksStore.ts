9ba532c8f6a7507c0d930ebfadbb422941597c8e
Carlos Martinez <carlosde362@gmail.com>
2022-08-24 20:25:01 +0200

    added action to remove, restore and toogle complete tasks

--- src/stores/TasksStore.ts
+++ src/stores/TasksStore.ts
@@ -11,10 +11,38 @@ const useStoreTasks = defineStore('tasks', {
     };
   },
   actions: {
+    updateLocalStorage(tasks: Task[]) {
+      setLocalStorage(KeysLocalStorage.TASKS, tasks);
+    },
     addTask(contentNewTask: string) {
       const newTask = new Task(contentNewTask);
-      setLocalStorage(KeysLocalStorage.TASKS, [...this.tasks, newTask]);
+      this.updateLocalStorage([...this.tasks, newTask]);
       this.tasks = [...this.tasks, newTask] as Task[];
+    },
+    removeTask(id: number) {
+      console.log(`remove ${id}`);
+      if (this.tasks.length === 0 || !this.tasks[id]) {
+        return;
+      }
+
+      this.tasks[id].isRemove = true;
+      this.updateLocalStorage(this.tasks);
+    },
+    restoreTask(id: number) {
+      if (this.tasks.length === 0 || !this.tasks[id]) {
+        return;
+      }
+
+      this.tasks[id].isRemove = false;
+      this.updateLocalStorage(this.tasks);
+    },
+    TooglemarkAsComplete(id: number) {
+      if (this.tasks.length || !this.tasks[id]) {
+        return;
+      }
+
+      this.tasks[id].isComplete = !this.tasks[id].isComplete;
+      this.updateLocalStorage(this.tasks);
     },
   },
   getters: {
