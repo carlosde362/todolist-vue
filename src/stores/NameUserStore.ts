2167875dc60fb1cdc6bef82e50c63eeb38667f38
Carlos Martinez <carlosde362@gmail.com>
2022-08-24 20:33:26 +0200

    added user store

---
+++ src/stores/NameUserStore.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia';
+import { getLocalStorage, setLocalStorage } from '../utils/utils';
+import KeysLocalStorage from '../utils/KeysLocalStorage';
+
+const useStoreNameUser = defineStore('nameUser', {
+  state() {
+    return {
+      nameUser: getLocalStorage<string>(KeysLocalStorage.USER_NAME) ?? '',
+    };
+  },
+
+  actions: {
+    setNameUser(nameUser: string) {
+      console.log(nameUser);
+      this.nameUser = nameUser;
+      setLocalStorage(KeysLocalStorage.USER_NAME, nameUser.trim());
+    },
+  },
+  getters: {
+    getCurrentName(): string {
+      return this.nameUser.trim();
+    },
+  },
+});
+
+export default useStoreNameUser;
