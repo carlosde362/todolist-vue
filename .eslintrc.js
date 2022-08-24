bc43d9d5f2f29ecc1793fd359af249f00a2746f0
Carlos Martinez <carlosde362@gmail.com>
2022-08-24 20:35:21 +0200

    change options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-    env: {
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        "plugin:vue/vue3-recommended",
-        "prettier"
-    ],
-    rules: {
-        "vue/require-default-prop": "off",
-    }
-}
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'vue/require-default-prop': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
