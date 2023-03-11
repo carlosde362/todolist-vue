3f14beda233c80d5e8ab00f2b1aac15f6f94f286
Carlos Martinez <carlosde362@gmail.com>
2023-03-11 12:22:20 +0100

    Fix: name user input

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,13 +2,26 @@
   <section class="w-full flex flex-row gap-2 items-center text-2xl">
     <span class="font-800">Hola!,</span>
     <input
+      v-if="isEdit"
+      v-model="valueInput.name"
       type="text"
       autocomplete="off"
       spellcheck="false"
-      v-model="valueInput.name"
-      @change="changeName(valueInput.name)"
       placeholder="Pon tu nombre..."
       class="bg-transparent outline-none"
+      @mouseenter="() => isEdit = true"
+      @mouseleave="() => isEdit = false"
+      @keyup.enter="() => isEdit = false"
+      @change="changeName(valueInput.name)"
+    />
+    <input
+      v-if="!isEdit"
+      type="text"
+      placeholder="Pon tu nombre..."
+      autocomplete="off"
+      :value="valueName"
+      class="bg-transparent outline-none"
+      @click="() => isEdit = true"
     />
   </section>
 </template>
@@ -26,9 +39,15 @@ export default defineComponent({
     return {
       valueInput: modelInputName,
       value: computed(() => (modelInputName.name = userStore.getCurrentName)),
+      valueName: computed(() => userStore.getCurrentName),
       changeName: (newName: string) => userStore.setNameUser(newName),
     };
   },
+  data() {
+    return {
+      isEdit: false,
+    }
+  }
 });
 </script>
 
